dd94848d03d9461b417b745de4b3a6ae196d0535
Baber Ibrar <baberibrar@gmail.com>
2025-04-28 13:16:05 +0500

    refactor NetflixSeries component to display genre and update practices component to show student count

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,4 +1,22 @@
+const returGenre = () => {
+  const genre = "RomCom";
+  return genre;
+}
+
 const NetflixSeries = () => {
+  const name = "Game of Thrones";
+  const summary = 
+        `The series is set on the fictional continents of Westeros and Essos and
+        interweaves several plot lines with a large ensemble cast. The first
+        narrative arc follows a civil war among several noble houses for the
+        Iron Throne of the Seven Kingdoms; the second covers the attempts to
+        reclaim the throne by the exiled last scion of the realm's deposed
+        ruling dynasty; the third chronicles the rising threat of the impending
+        winter and the legendary creatures and fierce peoples of the North.`;
+  let age = 18;
+  let canWatch = "Not Available";
+  if (age >= 18) canWatch = "Watch Now";
+
   return (
     <div>
       <div>
@@ -9,57 +27,11 @@ const NetflixSeries = () => {
           height="40%"
         />
       </div>
-      <h2>Name: Game of Thrones</h2>
+      <h2>Name: {name}</h2>
       <h3>Rating: 9.3/10</h3>
-      <p>
-        The series is set on the fictional continents of Westeros and Essos and
-        interweaves several plot lines with a large ensemble cast. The first
-        narrative arc follows a civil war among several noble houses for the
-        Iron Throne of the Seven Kingdoms; the second covers the attempts to
-        reclaim the throne by the exiled last scion of the realm's deposed
-        ruling dynasty; the third chronicles the rising threat of the impending
-        winter and the legendary creatures and fierce peoples of the North.
-      </p>
-
-      <div>
-        <img
-          src="breaking-bad.jpg"
-          alt="breaking-bad.jpg"
-          width="40%"
-          height="40%"
-        />
-      </div>
-      <h2>Name: Breaking Bad</h2>
-      <h3>Rating: 9.5/10</h3>
-      <p>
-        Breaking Bad is an American neo-Western crime drama television series
-        created and produced by Vince Gilligan. The show aired on AMC from
-        January 20, 2008, to September 29, 2013, consisting of five seasons for
-        a total of 62 episodes. It was set and filmed in Albuquerque, New
-        Mexico, and tells the story of Walter White (Bryan Cranston), an
-        underpaid and dispirited high school chemistry teacher who is struggling
-        with a recent diagnosis of stage-three lung cancer. White turns to a
-        life of crime, partnering with a former student, Jesse Pinkman (Aaron
-        Paul), to produce and distribute methamphetamine to secure his family's
-        financial future before he dies, while navigating the dangers of the
-        criminal underworld.
-      </p>
-
-      <div>
-        <img src="sherlock.jpg" alt="sherlock.jpg" width="40%" height="40%" />
-      </div>
-      <h2>Name: Sherlock</h2>
-      <h3>Rating: 9.1/10</h3>
-      <p>
-        Sherlock is a British crime television series based on Sir Arthur Conan
-        Doyle's Sherlock Holmes detective stories. Created by Steven Moffat and
-        Mark Gatiss, it stars Benedict Cumberbatch as Sherlock Holmes and Martin
-        Freeman as Doctor John Watson. Thirteen episodes have been produced,
-        with four three-part series airing from 2010 to 2017, and a special
-        episode that aired on 1 January 2016. The series is set in the present
-        day, while the one-off special features a Victorian period fantasy
-        resembling the original Holmes stories.
-      </p>
+      <h4>Genre: {returGenre()}</h4>
+      <p>{summary}</p>
+      <button> {canWatch} </button>
     </div>
   );
 };
---
+++ src/components/Practices.jsx
@@ -0,0 +1,9 @@
+export const Practices = () => {
+    const student = [];
+    return (
+        <>
+        <p>{student.length && "No student found"}</p>
+        <p>Number of students: {student.length}</p>
+        </>
+    );
+};
